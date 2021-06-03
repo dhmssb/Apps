3f5b39784130fe0488f50c1022fb76c03a85c90a
dhms <dhmssb@gmail.com>
2021-06-03 13:42:07 +0700

    setting db

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+const express = require ('express')
+const bodyParser = require('body-parser')
+const cors = require ('cors')
+
+const app = express()
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cors({
+    origin: '*'
+}))
+
+// app.use('/comment', commentRoutes)
+// app.use('/article', articleRoutes)
+
+module.exports = app
