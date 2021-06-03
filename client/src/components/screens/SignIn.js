c0aa33913e76e70e9df5df5b479a7e9887fc8265
dhms <dhmssb@gmail.com>
2021-06-03 22:20:44 +0700

    signin/up page

--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-
 const SignIn = () => {
     return(
         <div className='mycard'>
             <div className="card auth-card input-fields">
                 <h2>InstApp</h2>
-                <input 
-                type='text'
-                placeholder='name'
-                />
                 <input 
                 type='text'
                 placeholder='email'
@@ -20,10 +15,10 @@ const SignIn = () => {
                 placeholder='password'
                 />
                 <button className="btn waves-effect waves-light #039be5 #0d47a1 blue darken-2">
-                    Signup
+                    Login
                 </button>
                 <h6>
-                    <Link to='/signin'>Already have an account</Link>
+                    <Link to='/signup'>Dont have an account ?</Link>
                 </h6>
             </div>
         </div>
