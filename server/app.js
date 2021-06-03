b6b1a856937dbaab1b904454734534baf4ddec58
dhms <dhmssb@gmail.com>
2021-06-03 13:22:49 +0700

    initiate

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require ('express')
+const app = express()
+const PORT = 5000
+
+const customMiddleware = (req,res,next) => {
+    console.log('middleware executed!!')
+    next()
+}
+
+app.use(customMiddleware)
+
+app.get('/'),(req,res) => {
+    res.send('hello world')
+}
+
+app.listen(PORT,() => {
+    console.log('server is running on', PORT)
+})
