6dc523e42768571fc4d5b381c11837b5cc59ef4a
dhms <dhmssb@gmail.com>
2021-06-03 21:37:27 +0700

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import NavBar from './components/Navbar'
+import './App.css'
+import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './components/screens/Home'
+import Signin from './components/screens/Login'
+import Profile from './components/screens/Profile'
+import Signup from './components/screens/Signup'
 
 function App() {
   return (
-    <h1>hello</h1>
+    <BrowserRouter>
+      <NavBar />
+      <Route exact path ='/'>
+        <Home />
+      </Route>
+      <Route path ='/signin'>
+        <Signin />
+      </Route>
+      <Route path ='/profile'>
+        <Profile />
+      </Route>
+      <Route path ='/signup'>
+        <Signup />
+      </Route>
+    </BrowserRouter>
   );
 }
 
