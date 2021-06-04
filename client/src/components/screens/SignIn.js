54453bbe755ce2399ef37f4550a813dfdd9baf36
dhms <dhmssb@gmail.com>
2021-06-05 00:09:38 +0700

    signin/up

--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -1,7 +1,42 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React,{useState} from 'react'
+import {Link, useHistory} from 'react-router-dom'
+import M from 'materialize-css'
 
 const SignIn = () => {
+    const history = useHistory()
+    const[password,setPassword] = useState('')
+    const[email,setEmail] = useState('')
+    const PostData =()=>{
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+           M.toast({html:'Invalid email', classes:'#c62828 red darken-3'})
+            return
+        }
+        fetch('http://localhost:5000/signin',{
+            method: 'post',
+            headers:{
+                'Content-Type' : 'application/json' 
+            },
+            body:JSON.stringify({
+                password,
+                email
+            })
+        }).then(res => res.json())
+        .then(data =>{
+            console.log(data)
+            if(data.message === 'Succesfully login'){
+                M.toast({html: data.message})
+                history.push('/')
+            }else{
+                M.toast({html: data.message, classes:'#c62828 red darken-3'})
+                
+            }
+        })
+        .catch(err=> {
+            console.log(err)
+        })
+
+    }
+
     return(
         <div className='mycard'>
             <div className="card auth-card input-fields">
@@ -9,12 +44,18 @@ const SignIn = () => {
                 <input 
                 type='text'
                 placeholder='email'
+                value= {email}
+                onChange={(e) => setEmail(e.target.value)}
                 />
                <input 
                 type='text'
                 placeholder='password'
+                value= {password}
+                onChange={(e) => setPassword(e.target.value)}
                 />
-                <button className="btn waves-effect waves-light #039be5 #0d47a1 blue darken-2">
+                <button className="btn waves-effect waves-light #039be5 #0d47a1 blue darken-2"
+                onClick= {() => PostData()}
+                >
                     Login
                 </button>
                 <h6>
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,8 +1,45 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React,{useState} from 'react'
+import {Link, useHistory} from 'react-router-dom'
+import M from 'materialize-css'
 
 
 const SignIn = () => {
+    const history = useHistory()
+    const[name,setName] = useState('')
+    const[password,setPassword] = useState('')
+    const[email,setEmail] = useState('')
+    const PostData =()=>{
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+           M.toast({html:'Invalid email', classes:'#c62828 red darken-3'})
+            return
+        }
+        fetch('http://localhost:5000/signup',{
+            method: 'post',
+            headers:{
+                'Content-Type' : 'application/json' 
+            },
+            body:JSON.stringify({
+                name,
+                password,
+                email
+            })
+        }).then(res => res.json())
+        .then(data =>{
+            console.log(data)
+            if(data.message === 'Succesfully login'){
+                M.toast({html: data.message})
+                history.push('/signin')
+            }else{
+                M.toast({html: data.message, classes:'#c62828 red darken-3'})
+                
+            }
+        })
+        .catch(err=> {
+            console.log(err)
+        })
+
+    }
+
     return(
         <div className='mycard'>
             <div className="card auth-card input-fields">
@@ -10,16 +47,24 @@ const SignIn = () => {
                 <input 
                 type='text'
                 placeholder='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 />
                 <input 
                 type='text'
                 placeholder='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 />
                <input 
                 type='text'
                 placeholder='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 />
-                <button className="btn waves-effect waves-light #039be5 #0d47a1 blue darken-2">
+                <button className="btn waves-effect waves-light #039be5 #0d47a1 blue darken-2"
+                onClick={() => PostData()}
+                >
                     Signup
                 </button>
                 <h6>
